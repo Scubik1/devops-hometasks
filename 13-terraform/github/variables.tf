27b522a7ca02f55f058a3a9ba6725417352e992f
Scubik1 <138704511+Scubik1@users.noreply.github.com>
2023-09-03 18:51:30 +0300

    Update 13-terraform/github/variables.tf

--- 13-terraform/github/variables.tf
+++ 13-terraform/github/variables.tf
@@ -1,15 +1,3 @@
 variable "tf_token" {
     type = string
 }
-
-# variable "files" {
-#   default = [
-#     "providers.tf",
-#     "locals.tf",
-#     "variables.tf",
-#     "deployment.tf",
-#     "service.tf",
-#     "ingress.tf",
-#     "github.tf",
-#   ]
-# }
